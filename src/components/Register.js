d99dd6ed3ee4d8c61c385b881962a231faa59a79
Matt Basile <matthewbasile2@gmail.com>
2019-02-07 14:28:35 -0500

    final registration copy.

--- src/components/Register.js
+++ src/components/Register.js
@@ -6,10 +6,6 @@ export default function Register(props) {
   return (
     <div className="w-full mx-auto flex flex-col justify-center h-screen">
     <AuthRegister {...props}/>
-      
-
-     
-
       <p className="text-center text-grey text-xs">
         ©{(new Date()).getFullYear()} Guidr. All rights reserved.
       </p>
